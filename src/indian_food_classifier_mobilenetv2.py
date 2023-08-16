51f20575b73ac72ed831c87116e7574ec6362aef
Chandrasekar Sivaraman <chandrasiva1993@gmail.com>
2023-08-16 19:38:19 +0200

    added training script mobilenet v2 model

--- src/indian_food_classifier_mobilenetv2.py
+++ src/indian_food_classifier_mobilenetv2.py
@@ -39,60 +39,16 @@ tf.test.is_gpu_available()
 # Create a structured dataset from raw dataset
 #%%
 # From the raw path we can make a list of all the categories or classes by listing the folder names
-category_list = os.listdir(os.path.join(os.getcwd(), os.pardir), 'images', 'train'))
+root_dir = os.path.abspath(os.path.join(os.getcwd(), os.pardir))
+images_dir = os.path.join(root_dir,'images')
 
-# Set the name of root directory of our final structured dataset
-root_dir = 'images'
-
-# # Just in case we delete a pre-existing folder for the root directory
-# if os.path.exists(os.path.join(os.getcwd(), root_dir)):
-#     shutil.rmtree(os.path.join(os.getcwd(), root_dir))
-
-# # Create train validation and test folders inside root directory
-# # Loop through each category in the list
-# for i in category_list:
-    
-#     # For each category create a folder within train, validation and test folders respectively
-#     if not os.path.exists(f'{root_dir}/train/{i}'):
-#         os.makedirs(f'{root_dir}/train/{i}')
-#     if not os.path.exists(f'{root_dir}/validation/{i}'):
-#         os.makedirs(f'{root_dir}/validation/{i}')
-#     if not os.path.exists(f'{root_dir}/test/{i}'):
-#         os.makedirs(f'{root_dir}/test/{i}')
-
-#     source = raw_data_path + '/' + i
-
-#     allFileNames = os.listdir(source)
-
-#     np.random.shuffle(allFileNames)
-
-#     test_split_ratio = 0.3
-#     test_len = int(len(allFileNames)*test_split_ratio)
-#     train_FileNames = allFileNames[:-test_len]
-#     temp_test_FileNames = allFileNames[-test_len:]
-
-#     validation_split_ratio = 0.5
-#     validation_len = int(len(temp_test_FileNames)*validation_split_ratio)
-
-#     test_FileNames = temp_test_FileNames[:-validation_len]
-#     validation_FileNames = temp_test_FileNames[-validation_len:]
-
-#     for name in train_FileNames:
-#         pass
-#         shutil.copy(f'{source}/{name}', f'{root_dir}/train/{i}')
-
-#     for name in test_FileNames:
-#         shutil.copy(f'{source}/{name}', f'{root_dir}/test/{i}')
-
-#     for name in validation_FileNames:
-#         shutil.copy(f'{source}/{name}', f'{root_dir}/validation/{i}')
-
+category_list = os.listdir(os.path.join(images_dir, 'train'))
 
 # %%
 #Setting dataset path for train and test sets 
-train_path = f"{root_dir}/train"
-validation_path = f"{root_dir}/validation"
-test_path = f"{root_dir}/test"
+train_path = f"{images_dir}/train"
+validation_path = f"{images_dir}/validation"
+test_path = f"{images_dir}/test"
 
 # %% [markdwon]
 # Image Data Generator is used to augment existing images to create variations of the image
@@ -224,7 +180,7 @@ model.compile(loss='categorical_crossentropy',
 
 
 # %%
-checkpoint = ModelCheckpoint(filepath='mobilenetv2.h5', 
+checkpoint = ModelCheckpoint(filepath=os.path.join(root_dir, 'exports', 'mobilenetv2.h5'), 
                                verbose=2, 
                                save_best_only=True)
 callbacks = [checkpoint]
---
+++ src/train.py
@@ -0,0 +1,189 @@
+#%%
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+import cv2
+
+from datetime import datetime
+import matplotlib.pyplot as plt
+import matplotlib.image as img
+import os
+import pandas as pd
+import numpy as np
+from pathlib import Path
+import shutil
+import warnings
+warnings.filterwarnings("ignore")
+
+#%%
+gpus = tf.config.experimental.list_physical_devices('GPU')
+print("Num GPUs Available: ", len(gpus))
+
+for gpu in gpus:
+    tf.config.experimental.set_memory_growth(gpu, True)
+
+
+tf.test.is_gpu_available()
+
+
+#%%
+# From the raw path we can make a list of all the categories or classes by listing the folder names
+root_dir = os.path.abspath(os.path.join(os.getcwd(), os.pardir))
+images_dir = os.path.join(root_dir,'images')
+
+category_list = os.listdir(os.path.join(images_dir, 'train'))
+
+# %%
+#Setting dataset path for train and test sets 
+train_path = f"{images_dir}/train"
+validation_path = f"{images_dir}/validation"
+test_path = f"{images_dir}/test"
+
+#%%
+
+# Image Data Generator is used to augment existing images to create variations of the image
+# By using this method we can generate a bigger dataset than the one we have by augmenting and creating new images from existing ones
+
+batch_size = 32
+IMG_WIDTH = 224
+IMG_HEIGHT = 224
+
+train_datagen = ImageDataGenerator(
+    rescale = 1./255,
+    rotation_range=40,
+    zoom_range=0.2,
+    horizontal_flip=True)
+
+
+#%%
+train_set = train_datagen.flow_from_directory(train_path,
+                                              target_size = (224, 224),
+                                              batch_size = 32,
+                                              class_mode = 'categorical')
+
+
+# validtion Data Augmentation 
+validation_datagen = ImageDataGenerator(rescale=1./255)
+
+
+
+validation_set = validation_datagen.flow_from_directory(validation_path,
+                                                        target_size = (224, 224),
+                                                        batch_size = 32,
+                                                        class_mode = 'categorical')
+
+
+
+
+# Import TF Requirements
+#%%
+from tensorflow.keras.models import Model
+from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2
+from tensorflow.keras import optimizers
+from tensorflow.keras.callbacks import ModelCheckpoint
+
+IMAGE_SIZE = [224, 224, 3]
+base_model = MobileNetV2(input_shape=IMAGE_SIZE, weights='imagenet', include_top=False)
+
+base_model.trainable = False
+base_model.summary()
+
+# %%
+from keras import Sequential
+from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, GlobalAveragePooling2D
+
+global_average_layer = GlobalAveragePooling2D()
+
+
+
+#%%
+num_classes = len(category_list)
+prediction_layer = Dense(num_classes, activation='softmax')
+
+#%%
+
+
+model = Sequential([
+   base_model,
+   global_average_layer,
+   prediction_layer
+])
+
+model.summary()
+
+#%%
+# We initialize our parameters for training such as the optimizer, loss and learning rates
+
+adam = optimizers.Adam()
+model.compile(loss='categorical_crossentropy',
+              optimizer=adam,
+              metrics=['accuracy'])
+
+
+# %%
+checkpoint = ModelCheckpoint(filepath=os.path.join(root_dir, 'exports', 'mobilenetv2.h5'), 
+                               verbose=2, 
+                               save_best_only=True)
+callbacks = [checkpoint]
+start = datetime.now()
+
+history = model.fit(train_set,
+                      validation_data=validation_set,
+                      epochs=20,
+                      validation_steps=len(validation_set),
+                      callbacks=callbacks, 
+                      verbose=2)
+
+
+duration = datetime.now() - start
+print("Training completed in time: ", duration)
+
+
+
+
+
+
+
+
+# %%
+### FINE TUNING ###
+## UNFREEZING MOBILENET V2 BASE LAYERS ###
+
+# The base_model parameters are unfrozen to allow fine-tuning of the entire model
+base_model.trainable = True
+model.summary()
+
+# %%
+# Adam optimizer is created with a lower learning rate (1e-5) for fine-tuning.
+
+#%%
+adam = optimizers.Adam(1e-5)
+model.compile(loss='categorical_crossentropy',
+              optimizer=adam,
+              metrics=['accuracy'])
+
+#%%
+
+checkpoint = ModelCheckpoint(filepath=os.path.join(root_dir, 'exports', 'mobilenetv2.h5'), 
+                               verbose=2, save_best_only=True)
+callbacks = [checkpoint]
+start = datetime.now()
+
+history_finetuned = model.fit(train_set,
+                      validation_data=validation_set,
+                      epochs=50,
+                      validation_steps=len(validation_set),
+                      callbacks=callbacks, 
+                      verbose=2)
+
+
+
+duration = datetime.now() - start
+print("Training completed in time: ", duration)
+
+
+#%%
+# Open a text file in write mode
+with open(os.path.join(root_dir, 'exports', 'labels_list.txt'), 'w') as file:
+    for item in category_list:
+        file.write(str(item) + '\n')
